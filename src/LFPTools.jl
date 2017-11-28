404a68c53bdd782bf37e9fc6f11d00ba79ac5547
roger <roger.herikstad@gmail.com>
2017-11-28 15:03:35 +0800

    Slight refactoring

--- src/LFPTools.jl
+++ src/LFPTools.jl
@@ -28,8 +28,21 @@ Remove line noise at frequency `f0` Hz from the signal `Y`.
 """
 function remove_linenoise(Y, f0=50.0, fs=30_000.0, x=(0.0:1/fs:(length(Y)-1)/fs))
     pp = estimate_sinusoid(Y, f0, fs, x)
-    Y2 = sinfunc(x, f0, pp)
-    Y-Y2, pp
+    Y2 = copy(Y)
+    remove_linenoise!(Y2, pp, f0, fs)
+    Y2, pp
+end
+
+"""
+Remove line noise at frequency `f0` in-place using the fitted parameters `pp`.
+"""
+function remove_linenoise!(Y, pp, f0=50.0, fs=30_000.0)
+    dt = 1/fs
+    x = 0.0
+    for i in 1:length(Y)
+       Y[i] -= sinfunc(x,f0, pp)
+       x += dt
+    end
 end
 
 end#module
