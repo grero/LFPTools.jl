d581c52be532aed81a6385978ef6879ee3e03ac9
roger <roger.herikstad@gmail.com>
2017-12-02 23:27:57 +0800

    Chnages from default to custom RNG

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using Base.Test
 using LFPTools
 
+const RNG = MersenneTwister(UInt32[1234])
 """
 Create a signal with embedded line noise
 """
@@ -11,18 +12,18 @@ function generate_data()
     Y = zeros(length(x))
     #random walk
     for i in 2:length(x)
-        Y[i] = Y[i-1] + 0.1*randn()
+        Y[i] = Y[i-1] + 0.1*randn(RNG)
     end
     Y .+= 1.0*sin.(2*pi*50.0*x)
-    
+
     #create a gamma modulation lasting 100ms at random points
     tt = 0.0:1/fs:0.1
     V = 5.0*exp.(-(0.05-tt).^2/(2*0.02^2)).*sin.(2*pi*40.0*tt)
     t0 = Int64[]
-    _t0 = rand(1:2*length(V)) 
+    _t0 = rand(RNG, 1:2*length(V))
     for i in 1:length(x)-length(V)
         if i - _t0 >= 2*length(V)
-            _t0 = rand(i:i+2*length(V)) 
+            _t0 = rand(RNG, i:i+2*length(V))
             push!(t0, _t0)
             Y[_t0:_t0+length(V)-1] .+= V
         end
@@ -33,7 +34,7 @@ end
 function test()
     srand(1234)
     x, Y, t0 = generate_data()
-    Y2,pp = LFPTools.remove_linenoise(Y)
+    Y2,pp = LFPTools.denoise(Y)
     loss = norm(Y-Y2)
     @test loss ≈ 370.54919665078774
     @test pp[1] ≈ 0.0002496514539056982
