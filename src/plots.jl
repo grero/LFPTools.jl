16e7360316aeb254e7b407b616b0bc8ea5e6435a
roger <roger.herikstad@gmail.com>
2018-11-09 13:32:58 +0800

    Updates plots

--- src/plots.jl
+++ src/plots.jl
@@ -26,7 +26,7 @@ function plot_lfp(Xβ::Matrix{Float64}, Xγ::Matrix{Float64}, x;labels=["0.1 - 1
     fig
 end
 
-function plot_spectrogram(Y::Vector{T}, args...;kvs...) where T <: Real
+function plot_spectrogram(Y::Union{Array{T}, DSP.Periodograms.Spectrogram}, args...;kvs...) where T <: Real
     fig = plt[:figure]()
     ax = fig[:add_subplot](111)
     plot_spectrogram(ax, Y, args...;kvs...)
@@ -36,8 +36,28 @@ end
 """
 Plot the time-frequency power spectrum of the signal `Y` sampled at sampling rate `fs` using the matplotlib axes object `ax`. The parameter `t0` indicates the time in seconds of the first point in `Y`. The optional parameeter `logscale` indicates wheter to plot the log of the power, and `interpolation` specifies an interpolation method (defaults to "sinc") to be applied to the image.
 """
-function plot_spectrogram(ax, Y::Vector{T}, t0::Real, fs=1000;fmax=fs/2, logscale=false, interpolation="sinc",events=Float64[]) where T <: Real
+function plot_spectrogram(ax, Y::Vector{T}, t0::Real, fs=1000;kvs...) where T <: Real
     PP =  spectrogram(Y, fs=fs)
+    plot_spectrogram(ax, PP, t0, fs;kvs...)
+end
+
+function plot_spectrogram(ax, Y::Array{T,2}, t0::Real, fs=1000;kvs...) where T <: Real
+    nbins, ntrials = size(Y)
+    PP = spectrogram(Y[:,1], fs=fs)
+    μ = PP.power
+    σ² = PP.power.*PP.power
+    for ti in 2:ntrials
+        PP = spectrogram(Y[:,ti], fs=fs)
+        pp = PP.power
+        μ .+= pp
+        σ² .+= pp.*pp
+    end
+    μ ./= ntrials
+    σ² .= sqrt.(σ²/ntrials - μ.*μ)
+    μ, σ², PP.freq, PP.time
+end
+
+function plot_spectrogram(ax, PP::DSP.Periodograms.Spectrogram, t0::Real, fs=1000;fmax=fs/2, logscale=false, interpolation="sinc",events=Float64[])
     fidx = find(PP.freq .<= fmax)
     if logscale
         ppower = log10.(PP.power)
@@ -46,7 +66,7 @@ function plot_spectrogram(ax, Y::Vector{T}, t0::Real, fs=1000;fmax=fs/2, logscal
         ppower = PP.power
         clabel= "Power"
     end
-    II = ax[:imshow](ppower[fidx,:];aspect="auto", extent=(t0, t0 + length(Y)/fs , PP.freq[1], PP.freq[fidx[end]]), origin="lower",interpolation=interpolation)
+    II = ax[:imshow](ppower[fidx,:];aspect="auto", extent=(t0, t0 + PP.time[end], PP.freq[1], PP.freq[fidx[end]]), origin="lower",interpolation=interpolation)
     for ee in events
         ax[:axvline](ee;color="w")
     end
